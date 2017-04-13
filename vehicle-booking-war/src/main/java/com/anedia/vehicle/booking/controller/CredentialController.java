9f174cdda6d0a49582c6d7722d47180519d42b59
xyadnep <yadab.nepal@wipro.com>
2017-04-13 13:46:48 +0200

    minor changes

--- vehicle-booking-war/src/main/java/com/anedia/vehicle/booking/controller/CredentialController.java
+++ vehicle-booking-war/src/main/java/com/anedia/vehicle/booking/controller/CredentialController.java
@@ -4,15 +4,24 @@ import com.anedia.vehicle.booking.entity.CredentialBean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Created by xyadnep on 4/12/17.
- */
 @Controller
 public class CredentialController {
 
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public CredentialBean login(CredentialBean credentialBean){
-        return null;
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public @ResponseBody CredentialBean login(){
+        CredentialBean credentialBean = new CredentialBean();
+        credentialBean.setUserID("sdfsdf");
+        credentialBean.setPassword("sdfsd");
+        return credentialBean;
+    }
+
+    @RequestMapping(value = "/login1", method = RequestMethod.GET)
+    public @ResponseBody CredentialBean login1(){
+        CredentialBean credentialBean = new CredentialBean();
+        credentialBean.setUserID("Sujatha");
+        credentialBean.setPassword("Aman");
+        return credentialBean;
     }
 }
